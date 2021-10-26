25376c4d0c06f4d4bb9e6986b5ab31a6633e3c6e
KirillMalko <malkokirill099@gmail.com>
2021-10-26 19:03:43 +0300

    fix: add some fitch

--- script/player.js
+++ script/player.js
@@ -16,22 +16,22 @@ let randomInt = randomInteger(1, 4)
 const playList = [
     {
         title: 'Aqua Caelestis',
-        src: '../assets/sounds/Aqua-Caelestis.mp3',
+        src: './assets/sounds/Aqua-Caelestis.mp3',
         duration: '00:58'
     },
     {
         title: 'River Flows In You',
-        src: '../assets/sounds/River-Flows-In-You.mp3',
+        src: './assets/sounds/River-Flows-In-You.mp3',
         duration: '03:50'
     },
     {
         title: 'Ennio Morricone',
-        src: '../assets/sounds/Ennio-Morricone.mp3',
+        src: './assets/sounds/Ennio-Morricone.mp3',
         duration: '01:38'
     },
     {
         title: 'Summer Wind',
-        src: '../assets/sounds/Summer-Wind.mp3',
+        src: './assets/sounds/Summer-Wind.mp3',
         duration: '01:51'
     }
 
